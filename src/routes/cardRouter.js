6996a81c0c5ae6cadfbbaaa7df53c310a0a5671e
Denis Michailov <kamrad.reg@gmail.com>
2017-09-12 23:39:29 +1000

    findOne from mongodb

--- src/routes/cardRouter.js
+++ src/routes/cardRouter.js
@@ -1,8 +1,7 @@
-const express    = require('express');
-const cardRouter = express.Router();
+const express     = require('express');
+const cardRouter  = express.Router();
 const mongodb     = require('mongodb').MongoClient;
-// let   cards      = require('./cards');
-
+const objectId    = require('mongodb').ObjectID;
 
 let router = function(nav) {
 
@@ -13,12 +12,11 @@ let router = function(nav) {
         let collection = db.collection('cards')
       
         collection.find({}).toArray(function(err, results) {
-            res.render('cardListView', {
-              nav: nav,
-              cards: results
-            });
+          res.render('cardListView', {
+            nav: nav,
+            cards: results
+          });
         });
-
         // db.close();
       });
     });
@@ -31,17 +29,17 @@ let router = function(nav) {
 
   cardRouter.route('/:id')
     .get(function(req, res) {
+      let id  = new objectId(req.params.id);
+      let url = 'mongodb://localhost:27017/cardsApp';
       
-      let id   = req.params.id;
-      // вернуть все элементы массива с ID == id
-      let cardsFiltered = getCardByID(cards, id);
-      // так как id уникальны - взять первый элемент
-      // на самом деле нужно будет как-то по другому реализовать
-      let card = cardsFiltered[0];
-      
-      res.render('cardView', {
-        nav: nav,
-        card: card
+      mongodb.connect(url, function(err, db) {
+        let collection = db.collection('cards')
+        collection.findOne({_id: id}, function(err, results) {
+          res.render('cardView', {
+            nav: nav,
+            card: results
+          });
+        });
       });
 
     });
